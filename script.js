2a130819c01a4eb19b658a46197e973a9f8a2e66
Drewen Schoon <vibrationalenlightenment@gmail.com>
2025-05-18 18:19:32 -0700

    v13

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-window.onload = () => {
-    const button = document.getElementById("breakButton");
-  
-    if (button) {
-      button.addEventListener("click", () => {
-        document.body.classList.add("breaking");
-  
-        const blocker = document.querySelector('.fade-blocker');
-  
-        if (blocker) {
-          // Wait for the fade-blocker to fully fade in
-          blocker.addEventListener('transitionend', () => {
-            window.location.href = "SalesPage.html"; // or your next page
-          });
-        }
-      });
-    }
-  };
+
   
 
